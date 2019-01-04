64980b7eae4017ed0e76726e92f596104281fa60
Soulyana Lakew <soulyana.lakew@accenturefederal.com>
2019-01-03 19:19:14 -0500

    Connect Container

--- src/index.js
+++ src/index.js
@@ -2,12 +2,13 @@ import "babel-polyfill";
 import React from "react";
 import { render } from "react-dom";
 import configureStore from "./store/configureStore";
-import { Provider } from "react-redux";
+import { Provider } from "react-redux"; // higher order component that attaches our store to our react container components
 import { Router, browserHistory } from "react-router";
 import routes from "./routes";
 import "./styles/styles.css"; //Webpack can import CSS files too!
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 
+// instance of store
 const store = configureStore();
 
 render(
