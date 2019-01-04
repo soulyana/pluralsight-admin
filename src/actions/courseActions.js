00b2da27e9962907f806c2b3c4fe2708c8156783
Soulyana Lakew <soulyana.lakew@accenturefederal.com>
2019-01-03 20:42:53 -0500

    Create Load Courses Thunk

--- src/actions/courseActions.js
+++ src/actions/courseActions.js
@@ -1,6 +1,19 @@
 import * as types from './actionTypes';
+import courseApi from '../api/mockCourseApi';
 
-export function createCourse(course) {
+export function loadCoursesSuccess(courses) {
     // debugger;
-    return { type: types.CREATE_COURSE, course};
+    return { type: types.LOAD_COURSES_SUCCESS, courses};
+}
+
+
+
+export function loadCourses() {
+    return function(dispatch) {
+        return courseApi.getAllCourses().then(courses => {
+            dispatch(loadCoursesSuccess(courses));
+        }).catch(error => {
+            throw(error);
+        });
+    };
 }
